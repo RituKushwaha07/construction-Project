e6dfef2d47bd1355ad233f49bc449da7b4fe1373
Ritu Kushwaha <reetukushwah50@gmail.com>
2025-02-20 13:00:45 +0530

    files uploaded

---
+++ javascript/script.js
@@ -0,0 +1,106 @@
+
+<footer class="Husmokh">
+<div class="box1">
+    <h1>Contact Info</h1>
+    <p>A-220, Sector -69, Noida, UP-<br />201301, India</p>
+    <p>contact@nationaldrywall.in</p>
+    <p> +91 9312355510</p>
+</div>
+
+
+<div class="box2">
+    <h1>Contact Info</h1>
+    <p>Mon - Friday:<br />10:00 am to 06:00 pm</p>
+    <p>Satday: <br />10:00 am to 02:00 pm</p>
+    <p>Vacation:<br />All Sunday is our vacation</p>
+</div>
+
+
+
+<div class="box3">
+    <h1>Our Services</h1>
+    <p> Business</p>
+    <p> Evaluation</p>
+    <p> Work / Career</p>
+</div>
+
+
+
+<div class="box4">
+    <h1>Newsletter</h1>
+   <p>Frontent-Developer</p>
+    <p>Backend-Developer</p>
+    <p>Full-Stack Developer</p>
+</div>
+
+</footer>
+
+
+
+
+
+.Husmokh {
+    background-color: #2e85d6;
+    height: 500px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+
+}
+
+
+.Husmokh .box1 h1 {
+    margin-bottom: 20px;
+    margin-left: 100px;
+    
+
+}
+
+
+
+p {
+  margin-left: 100px;
+    line-height: 1.8rem;
+
+}
+
+
+
+/* box-2 */
+
+.Husmokh .box2 h1 {
+    margin-bottom: 20px;
+    margin-left: 100px;
+    
+
+}
+
+
+
+
+
+.Husmokh .box3 h1 {
+    margin-bottom: 20px;
+    margin-left: 100px;
+    
+
+}
+
+
+
+
+
+/* .Husmokh .box4 h1 {
+    margin-bottom: 20px;
+    margin-left: 100px;
+    
+
+} */
+
+
+
+
+
+
+
